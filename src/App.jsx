348b3aa18b73d82714c7323d173e290444bdf405
unknown <sergeiromanovic82@gmail.com>
2023-03-22 12:27:18 +0300

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./Components/SushiNavMenu";
+import './index.css';
+
+
+
+function App() {
+    return(
+        <div>
+            <NavBar/>
+        </div>
+    )
+    
+ 
+}
+
+export default App;
